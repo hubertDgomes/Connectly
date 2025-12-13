9f9818113632db27d1661fbce3ccf78f4fa3080e
Rasel_Ahamed <rasel.ahamed.dev@gmail.com>
2025-12-13 19:58:35 +0600

    h1 ADD korsi

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Container from '../Container'
 
 const Home = () => {
   return (
     <>
+    <Container>
+      <h1 className='text-3xl font-bold underline'>
+        Hello, welcome to the Home Page!
+      </h1>
+    </Container>
     </>
   )
 }
